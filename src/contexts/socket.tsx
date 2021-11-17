b272c11e7c11cc2d420c3c86935180417982b392
Brendan Kennedy <brenwken@gmail.com>
2021-11-17 17:38:05 -0500

    v0.0.23

--- src/contexts/socket.tsx
+++ src/contexts/socket.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { io } from 'socket.io-client'
 
+// 'http://foghorn-api.bken.io:3200'
 export const socket = io('http://foghorn-api.bken.io:3200')
 export const SocketContext = React.createContext({ socket })
