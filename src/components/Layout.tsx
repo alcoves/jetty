4f440921b4c8ab0d10fbe0ad4aa97e8e7d751876
rustyguts <9222118+rustyguts@users.noreply.github.com>
2021-12-09 01:40:23 -0500

    Wrap harbor route in private route component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom'
 import { Flex, Box } from '@chakra-ui/react'
 import { SocketContext } from '../contexts/socket'
 import React, { useContext, useEffect } from 'react'
+import PrivateRoute from './PrivateRoute'
 
 export default function Layout(): JSX.Element {
   const socket = useContext(SocketContext)
@@ -14,12 +15,14 @@ export default function Layout(): JSX.Element {
   }, [])
 
   return (
-    <Box>
-      <TitleBar />
-      <TopNavBar />
-      <Flex h='calc(100vh - 70px)'>
-        <Outlet />
-      </Flex>
-    </Box>
+    <PrivateRoute>
+      <Box>
+        <TitleBar />
+        <TopNavBar />
+        <Flex h='calc(100vh - 70px)'>
+          <Outlet />
+        </Flex>
+      </Box>
+    </PrivateRoute>
   )
 }
